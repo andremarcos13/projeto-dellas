5a6e04a9bdeca1a8ce1eeea8f5ae1207bd58be70
Andre Cesar <andrmgc@gmail.com>
2024-03-21 09:22:30 -0400

    removing chakra UI, restarting the project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import { useEffect } from "react";
-import LoginPage from "./pages/login-page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importe BrowserRouter e Routes
-import HomePage from "./pages/home-page";
-import AgendaPage from "./pages/agenda-page";
 
 function App() {
-  useEffect(() => {
-    localStorage.setItem("chakra-ui-color-mode", "dark");
-  }, []); // O array vazio assegura que o efeito só será executado uma vez, equivalente ao componentDidMount()
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        {/* <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/agenda" element={<AgendaPage />} />
+        <Route path={"/atendimento"} element={<Atendimento />} /> */}
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
-  </ChakraProvider>
-);
+root.render(<App />);
