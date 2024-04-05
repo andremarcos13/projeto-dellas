72ae25168dacb643de7ab3393920b7bda0a4c4df
Andre Cesar <andrmgc@gmail.com>
2024-04-05 14:42:59 -0400

    create new api

---
+++ src/apis/preco-venda-api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const fetchPrecoDeVenda = async ({ cliente, produto, qtd }) => {
+  try {
+    const response = await axios.get(
+      "https://dellascomercio146177.protheus.cloudtotvs.com.br:1566/rest/api/v1/tabela_preco",
+      {
+        params: {
+          empresa: "01",
+          filial: "01",
+          loja: "00",
+          cliente: cliente,
+          produto: produto,
+          quantidade: qtd,
+        },
+        headers: {
+          Authorization: "Basic bmV4dXMuZGV2OmRlbGxhc0BuZXh1cw==",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export default fetchPrecoDeVenda;
