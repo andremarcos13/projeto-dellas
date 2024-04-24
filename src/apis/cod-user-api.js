9c693de71fbaea5d47ca91823764c51fa8ef532a
Andre Cesar <andrmgc@gmail.com>
2024-04-24 09:56:42 -0400

    create api for fetching coduser

---
+++ src/apis/cod-user-api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const fetchCodUser = async (token) => {
+  try {
+    const response = await axios.get(
+      "https://dellascomercio146177.protheus.cloudtotvs.com.br:1566/rest/api/framework/v1/genericList",
+      {
+        params: {
+          alias: "SU7",
+          fields: "U7_NOME,U7_CODUSU",
+          Page: 1,
+          PageSize: 50,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export default fetchCodUser;
