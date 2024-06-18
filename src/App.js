0dc9b0115cf8f7f2573d61435b58a4ba21b644f6
Andre Cesar <andrmgc@gmail.com>
2024-06-18 11:13:49 -0400

    create route for new planned component, lead locator

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Im
 import HomePage from "./pages/home-page";
 import AgendaPage from "./pages/agenda-page";
 import AtendimentoPage from "./pages/atendimento-page";
+import Map from "./components/map";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/agenda" element={<AgendaPage />} />
         <Route path="/atendimento/:index" element={<AtendimentoPage />} />
+        <Route path="/maps" element={<Map />} />
       </Routes>
     </Router>
   );
